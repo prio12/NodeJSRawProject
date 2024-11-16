8245c51b3855ee0e849adfc9d9c70d323dc7b779
prio12 <maksudurprio.mp@gmail.com>
2024-11-17 00:24:55 +0600

    put method added for checks functionality

--- handler/routesHandlers/checkHandler.js
+++ handler/routesHandlers/checkHandler.js
@@ -143,15 +143,19 @@ handler._check.get = (requestProperties, callback) => {
             ? requestProperties.headersObject.token
             : false;
 
-           tokenVerify._token.verify(token,parseJSON(data).phone, (verifiedToken) =>{
+        tokenVerify._token.verify(
+          token,
+          parseJSON(data).phone,
+          (verifiedToken) => {
             if (verifiedToken) {
-              callback(200,parseJSON(data))
+              callback(200, parseJSON(data));
             } else {
               callback(403, {
-                error:"Unauthorized user!"
-              })
+                error: "Unauthorized user!",
+              });
             }
-           })
+          }
+        );
       } else {
         callback(500, {
           error: "There was a server side error!",
@@ -164,6 +168,100 @@ handler._check.get = (requestProperties, callback) => {
     });
   }
 };
-handler._check.put = (requestProperties, callback) => {};
+handler._check.put = (requestProperties, callback) => {
+  const id =
+    typeof requestProperties.body.id === "string" &&
+    requestProperties.body.id.trim().length === 20
+      ? requestProperties.body.id
+      : false;
+
+  let protocol =
+    typeof requestProperties.body.protocol === "string" &&
+    ["https", "http"].indexOf(requestProperties.body.protocol) > -1
+      ? requestProperties.body.protocol
+      : false;
+  let url =
+    typeof requestProperties.body.url === "string" &&
+    requestProperties.body.url.trim().length > 0
+      ? requestProperties.body.url
+      : false;
+  let method =
+    typeof requestProperties.body.method === "string" &&
+    ["GET", "POST", "PUT", "DELETE"].indexOf(requestProperties.body.method) > -1
+      ? requestProperties.body.method
+      : false;
+  let successCodes =
+    typeof requestProperties.body.successCodes === "object" &&
+    requestProperties.body.successCodes instanceof Array
+      ? requestProperties.body.successCodes
+      : false;
+
+  let timeOutSeconds =
+    typeof requestProperties.body.timeOutSeconds === "number" &&
+    requestProperties.body.timeOutSeconds % 1 === 0 &&
+    requestProperties.body.timeOutSeconds >= 1 &&
+    requestProperties.body.timeOutSeconds <= 5
+      ? requestProperties.body.timeOutSeconds
+      : false;
+
+  if (id) {
+    if (protocol || url || method || successCodes || timeOutSeconds) {
+      lib.read("checks",id, (data, err1) =>{
+        if (!err1 && data) {
+          let checkData = parseJSON(data);
+          let token =
+          typeof requestProperties.headersObject.token === "string"
+            ? requestProperties.headersObject.token
+            : false;
+            tokenVerify._token.verify(token,checkData.phone, (verifiedToken) =>{
+              if (verifiedToken) {
+                if (protocol) {
+                  checkData.protocol = protocol
+                }
+                if (protocol) {
+                  checkData.url = url
+                }
+                if (protocol) {
+                  checkData.method = method
+                }
+                if (protocol) {
+                  checkData.successCodes = successCodes
+                }
+                if (protocol) {
+                  checkData.timeOutSeconds = timeOutSeconds
+                }
+
+                lib.update("checks",id, checkData, (err2) =>{
+                  if(!err2) {
+                    callback(200, checkData)
+                  } else {
+                    callback(500, {
+                      error:"There was a problem in server side!"
+                    })
+                  }
+                })
+              } else {
+                callback(403, {
+                  error:"Authentication error !"
+                })
+              }
+            })
+        } else {
+          callback(500,{
+            error: "There was a problem in the server side!",
+          })
+        }
+      })
+    } else {
+      callback(400, {
+        error: "You must provide at least one field to update",
+      });
+    }
+  } else {
+    callback(400, {
+      error: "There was a problem in your request!",
+    });
+  }
+};
 handler._check.delete = (requestProperties, callback) => {};
 module.exports = handler;
